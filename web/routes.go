61d6b2c296445a9aa2a930ea328479f881f05125
Bryan Amundson <bryan@smartystreets.com>
2021-09-28 22:42:04 -0600

    Added more routes.

--- web/routes.go
+++ web/routes.go
@@ -13,6 +13,33 @@ func SetupRoutes(appConfig config.Config, controller *Controller) {
 	http.Handle("/v1/adults-without-calling", detour.New(controller.AdultsWithoutCalling))
 	http.Handle("/v1/eligible-adults", detour.New(controller.AdultsEligibleForCalling))
 	http.Handle("/v1/eligible-youth", detour.New(controller.YouthEligibleForCalling))
+	http.Handle("/v1/get-member", detour.New(controller.GetMemberRecord)) // member
+	http.Handle("/v1/organizations", detour.New(controller.OrganizationList))
+	http.Handle("/v1/callings", detour.New(controller.CallingList)) // org
 
 
 }
+
+/*
+func (this *Controller) MembersWithCallings() detour.Renderer {
+func (this *Controller) VacantCallingList(input *InputModel) detour.Renderer {
+func (this *Controller) AddCalling(input *InputModel) detour.Renderer {
+func (this *Controller) RemoveCalling(input *InputModel) detour.Renderer {
+func (this *Controller) UpdateCalling(input *InputModel) detour.Renderer {
+func (this *Controller) AddMemberToCalling(input *InputModel) detour.Renderer {
+func (this *Controller) MoveMemberToAnotherCalling(input *InputModel) detour.Renderer {
+func (this *Controller) RemoveMemberFromCalling(input *InputModel) detour.Renderer {
+func (this *Controller) Diff() detour.Renderer {
+func (this *Controller) ListTransactionFiles() detour.Renderer {
+func (this *Controller) LoadTransactions(input *InputModel) detour.Renderer {
+func (this *Controller) SaveTransactions(input *InputModel) detour.Renderer {
+func (this *Controller) UndoTransaction() detour.Renderer {
+func (this *Controller) RedoTransaction() detour.Renderer {
+
+func (this *Controller) LoadCallings() detour.Renderer {
+func (this *Controller) SaveCallings() detour.Renderer {
+func (this *Controller) LoadMembers() detour.Renderer {
+func (this *Controller) SaveMembers() detour.Renderer {
+parseMembers
+parseCallings
+ */
