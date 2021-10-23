aec2241f5acbc299015dd077da3555813488a014
Bryan Amundson <bryan@smartystreets.com>
2021-10-22 23:33:26 -0600

    Implemented a tree view of organizations and callings.

--- web/html/js/callorg.js
+++ web/html/js/callorg.js
@@ -1,18 +1,85 @@
-function parseRawData(endpoint) {
-	const rawData = document.getElementById("rawdata");
+window.onload = function () {
+	setupTree()
+};
+
+function setupTree() {
+	const wardOrgs = document.getElementById("ward-organizations");
+
 	const xhttp = new XMLHttpRequest();
 	xhttp.onreadystatechange = function () {
-		if (this.readyState === 4) {
-			let msg = this.responseText;
-			if (msg === "null\n") {
-				msg = "Import successful"
-			}
-			alert(msg);
+		if (this.readyState === 4 && this.status === 200) {
+			let jsonObject = JSON.parse(this.responseText);
+			jsonObject.forEach(function (calling) {
+				let container = document.getElementById(calling.Org);
+				if (container == null) {
+					container = document.createElement("li");
+					let caret = document.createElement("span");
+					caret.setAttribute("class", "caret");
+					caret.innerText = calling.Org;
+					container.appendChild(caret);
+					wardOrgs.appendChild(container);
+					let nested = document.createElement("ul");
+					nested.setAttribute("class", "nested");
+					nested.setAttribute("id", calling.Org);
+					container.appendChild(nested);
+					container = nested;
+				}
+
+				if (calling.SubOrg !== "") {
+					let subOrg = document.getElementById(calling.Org + calling.SubOrg);
+					if (subOrg == null) {
+						subOrg = document.createElement("li");
+						subOrg.setAttribute("id", calling.SubOrg)
+						let caret = document.createElement("span");
+						caret.setAttribute("class", "caret");
+						caret.innerText = calling.SubOrg;
+						subOrg.appendChild(caret);
+						container.appendChild(subOrg);
+						let nested = document.createElement("ul");
+						nested.setAttribute("class", "nested");
+						nested.setAttribute("id", calling.Org + calling.SubOrg);
+						subOrg.appendChild(nested);
+						container = nested;
+					} else {
+						container = subOrg;
+					}
+				}
+
+				let callingInfo = document.createElement("li");
+				callingInfo.innerText = calling.Name + " ; " + calling.Holder + " ; " + calling.PrintableSustained + " (" + calling.PrintableTimeInCalling + ")";
+				container.appendChild(callingInfo);
+			});
+
+			startTreeListeners()
 		}
 	};
-	xhttp.open("POST", "/v1/" + endpoint);
+
+	let url = "callings?org=All+Organizations";
+	xhttp.open("GET", "/v1/" + url);
 	xhttp.setRequestHeader("Content-type", "text/plain");
-	xhttp.send(rawData.value);
+	xhttp.send();
+}
+
+function treeCollapse() {
+	let toggler = document.getElementsByClassName("caret");
+	let i;
+
+	for (i = 0; i < toggler.length; i++) {
+		toggler[i].parentElement.querySelector(".nested").classList.toggle("active");
+		toggler[i].classList.toggle("caret-down");
+	}
+}
+
+function startTreeListeners() {
+	let toggler = document.getElementsByClassName("caret");
+	let i;
+
+	for (i = 0; i < toggler.length; i++) {
+		toggler[i].addEventListener("click", function () {
+			this.parentElement.querySelector(".nested").classList.toggle("active");
+			this.classList.toggle("caret-down");
+		});
+	}
 }
 
 function displayMembers(endpoint) {
@@ -41,36 +108,6 @@ function memberSelected(index) {
 	displayCallings("callings-for-member", "member", memberOptions[index].text);
 }
 
-function displayOrganizations(endpoint) {
-	const orgElement = document.getElementById("organizations");
-	orgElement.innerHTML = "";
-	let opt = document.createElement('option');
-	opt.value = "All Organizations";
-	opt.innerText = "All Organizations";
-	orgElement.appendChild(opt);
-
-	const xhttp = new XMLHttpRequest();
-	xhttp.onreadystatechange = function () {
-		if (this.readyState === 4 && this.status === 200) {
-			let jsonObject = JSON.parse(this.responseText)
-			jsonObject.forEach(function (member) {
-				let opt = document.createElement('option');
-				opt.value = member;
-				opt.innerText = member;
-				orgElement.appendChild(opt);
-			});
-		}
-	};
-	xhttp.open("GET", "/v1/" + endpoint);
-	xhttp.setRequestHeader("Content-type", "text/plain");
-	xhttp.send();
-}
-
-function orgSelected(index) {
-	let orgOptions = document.getElementById('organizations').options;
-	displayCallings("callings", "org", orgOptions[index].text)
-}
-
 function displayCallings(endpoint, argName, arg) {
 	const callingElement = document.getElementById("callings");
 	callingElement.innerHTML = "";
@@ -92,4 +129,21 @@ function displayCallings(endpoint, argName, arg) {
 	xhttp.open("GET", "/v1/" + url);
 	xhttp.setRequestHeader("Content-type", "text/plain");
 	xhttp.send();
+}
+
+function parseRawData(endpoint) {
+	const rawData = document.getElementById("rawdata");
+	const xhttp = new XMLHttpRequest();
+	xhttp.onreadystatechange = function () {
+		if (this.readyState === 4) {
+			let msg = this.responseText;
+			if (msg === "null\n") {
+				msg = "Import successful"
+			}
+			alert(msg);
+		}
+	};
+	xhttp.open("POST", "/v1/" + endpoint);
+	xhttp.setRequestHeader("Content-type", "text/plain");
+	xhttp.send(rawData.value);
 }
