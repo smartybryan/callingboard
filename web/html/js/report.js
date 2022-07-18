96d5d6a64736ff4d47eb0f7e60bc844edf02147a
Bryan Amundson <bryan@smartystreets.com>
2022-07-17 19:08:21 -0600

    Customize Release and Sustain table independently.

--- web/html/js/report.js
+++ web/html/js/report.js
@@ -8,12 +8,12 @@ function generateReport() {
 	let releases = getCallingDataFromContainer("releases");
 	let sustainings = getCallingDataFromContainer("sustainings");
 	let content = "<div>Printed " + dateTime + " from Callingboard.org</div>"
-	content += buildReportTable(
+	content += buildReleaseReportTable(
 		"We have released the following.",
 		"Those who wish to express appreciation for their service, please raise your hand.",
 		"Release", releases);
 	content += "<br><br>";
-	content += buildReportTable(
+	content += buildSustainReportTable(
 		"We have called the following and ask that you stand when your name is read, and remain standing until the vote is taken.",
 		"We propose that they be sustained. All in favor, please raise your hand. [Wait] Those opposed by the same sign.",
 		"Sustain", sustainings);
@@ -37,7 +37,37 @@ function getCallingDataFromContainer(id) {
 	return callingList;
 }
 
-function buildReportTable(pretable, posttable, title, callings) {
+function buildReleaseReportTable(pretable, posttable, title, callings) {
+	let content = `
+	<h2>` + title + `</h2>
+	<div class="report-verbiage">` + pretable + `</div>
+	<br>
+	<table class="report-table">
+	<thead>
+	<tr><th class="report-table"><strong>Member</strong></th>
+	<th class="report-table"><strong>Calling</strong></th>
+	<th class="report-table"  style="width: 150px;"><strong>Organization</strong></th>
+	<th class="report-table" style="width: 100px;"><strong>Released</strong></th>
+	<th class="report-table"><strong>Date</strong></th></tr>
+	</thead>
+	<tbody>`;
+
+	for (let call of callings) {
+		content += "<tr>";
+		content += '<td class="report-table">' + call.holder + "</td>";
+		content += '<td class="report-table">' + call.calling + "</td>";
+		content += '<td class="report-table">' + call.org + "</td>";
+		content += '<td class="report-table" style="text-align: center;">☐</td>';
+		content += '<td class="report-table"></td>';
+		content += "</tr>";
+	}
+	content += `</tbody></table>
+	<br>
+	<div class="report-verbiage">` + posttable + `</div>`;
+	return content
+}
+
+function buildSustainReportTable(pretable, posttable, title, callings) {
 	let content = `
 	<h2>` + title + `</h2>
 	<div class="report-verbiage">` + pretable + `</div>
